e83c5ff3e33ace1e2c2ae30f1db4f149f1e614db
Anye Web <frujuniorp@gmail.com>
2023-07-06 03:30:28 -0600

    V 1.0

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./Style.css";
+import DividerImg from "./images/pattern-divider-desktop.svg";
+import DiceImg from "./images/icon-dice.svg";
 
 function App() {
+  const [advice, setAdvice] = useState({});
+  const [randNumber, setRandNumber] = useState(1);
+
+  useEffect(() => {
+    async function getAdvice() {
+      const res = await fetch(
+        `https://api.adviceslip.com/advice/${randNumber}`
+      );
+      const data = await res.json();
+      setAdvice(data.slip);
+    }
+    getAdvice();
+  }, [randNumber]);
+
+  function getRandomNum(e) {
+    e.preventDefault();
+    const randNum = Math.floor(Math.random() * 200);
+    setRandNumber(randNum);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="content">
+        <h1>Advice #{advice.id}</h1>
+        <p className="content-info">“{advice.advice}”</p>
+
+        <img src={DividerImg} alt="" className="divider-img" />
+        <button onClick={getRandomNum} className="btn">
+          <img src={DiceImg} alt="" className="btn-img" />
+        </button>
+      </div>
     </div>
   );
 }
